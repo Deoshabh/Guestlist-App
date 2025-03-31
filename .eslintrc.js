7c2ff6909a1844bceb101ed2641e61e0f6481d7d
Deoshabh <gagan2111015@akgec.ac.in>
2025-04-01 02:04:12 +0530

    Resolve merge conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,48 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
     es6: true,
+    jest: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
   rules: {
-    'max-lines': ['error', {
-      max: 500,
-      skipBlankLines: true,
-      skipComments: true
-    }],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Add any custom rules here
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
   overrides: [
     {
-      files: ['**/*.test.js', '**/*.spec.js'],
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+      ],
       env: {
         jest: true,
       },
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,28 +1,58 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
   extends: [
-    '../.eslintrc.js',
+    'eslint:recommended',
     'react-app',
     'react-app/jest',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
-  plugins: ['react'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
   settings: {
     react: {
       version: 'detect',
     },
   },
   rules: {
+    // Combined rules from both files
     'react/prop-types': 'warn',
     'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'max-lines': ['error', {
       max: 500,
       skipBlankLines: true,
       skipComments: true
     }],
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
